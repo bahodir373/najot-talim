163cec4ba7b312b508be95bd57153a5aa0e1a1e4
Bahodir Husainov <157041779+bahodir373@users.noreply.github.com>
2024-09-23 21:31:01 +0500

    Add files via upload

---
+++ 3-oy/6-dars/amaliy.js
@@ -0,0 +1,72 @@
+// 2-masala
+
+// function myMathPow(num, degree) {
+//     return num**degree;
+// }
+
+// console.log(myMathPow(2,3));
+
+
+
+// 3-masala
+
+// function customMin(array) {
+//     let min = array[0];
+
+//     for (let element of array) {
+//         if (element < min) {
+//             min = element;
+//         }
+//     }
+
+//     return min;
+// }
+
+// console.log(customMin([1,2,0,4,5]));
+
+
+// 4-masala
+
+// function customMax(array) {
+//     let max = array[0];
+
+//     for (let element of array) {
+//         if (element > max) {
+//             max = element;
+//         }
+//     }
+
+//     return max;
+// }
+
+// console.log(customMax([1,2,0,7,6]));
+
+
+// 5-masala
+
+// function customFloor(num) {
+//     let floor = parseInt(num)
+
+//     return floor;
+// }
+
+// console.log(customFloor(1.6));
+
+
+
+// 6-masala
+
+// function customCeil(num) {
+//     let halfFloat = num - parseInt(num);
+//     let butunQismi = parseInt(num)
+
+//     if (halfFloat > 0) {
+//         return butunQismi += 1;
+//     } else{
+//         return num;
+//     }
+// }
+
+// console.log(customCeil(1.3));
+
+
